9d182cdea8a28092610c1caf9db6f421b8e6185a
asielp3D <asielmasespacio@gmail.com>
2023-11-29 11:02:19 +0100

    examen

--- Assets/Scripts/GroundSensor.cs
+++ Assets/Scripts/GroundSensor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GroundSensor : MonoBehaviour
 {
+    public static bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,29 @@ public class GroundSensor : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 6 )
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+         if(other.gameObject.layer == 6 )
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+         if(other.gameObject.layer == 6 )
+        {
+            isGrounded = false;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float horizontal;
+    public float _plyerSpeed = 5;
+    public float _jumpForce = 5;
+    Rigidbody2D _rbody;
+    Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
+    
     void Update()
     {
-        
+        Movement();
     }
+
+    void FixedUpdate()
+    {
+        _rbody.velocity = new Vector2 (horizontal * _plyerSpeed, _rbody.velocity.y);
+    }
+
+    void Movement()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        
+        if(horizontal < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if(horizontal > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    void Jump()
+    {
+        //_rbody.Addforce
+    }
+
+    // Victor te quiero no me suspendas <3
 }
